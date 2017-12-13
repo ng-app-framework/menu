5c8c3625ef4666ce69a3958628ab5a9dc542f2c2
Justin <justin@homeceuconnection.com>
2017-12-13 09:46:33 -0600

    Updating module

--- lib/polyfills.ts
+++ lib/polyfills.ts
@@ -1,7 +1,7 @@
 // This file includes polyfills needed by Angular 2 and is loaded before
 // the app. You can add your own extra polyfills to this file.
 
-import 'reflect-metadata';
+import 'rxjs/Rx';
 
 import 'core-js/es6/symbol';
 import 'core-js/es6/object';
